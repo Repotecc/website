acc5372e33bc9447bddfa5faa2fad56601ca7f56
Repotecc <60607848+Repotecc@users.noreply.github.com>
2021-08-26 08:09:19 +0100

    Update 404-page.js

--- src/components/404-page.js
+++ src/components/404-page.js
@@ -1,1 +1,21 @@
 
+// import logo from './../../assets/img/repotecc_logo.png';
+// import { Link } from 'react-router-dom';
+//import './App.css';
+
+function PageNotFound() {
+  return (
+
+    <div id="notfound">
+      <div class="notfound">
+        <div class="notfound-404">
+          <h1>4<span>0</span>4</h1>
+        </div>
+        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+        <a href="https://repotecc.com">home page</a>
+      </div>
+    </div>
+  );
+}
+
+export default PageNotFound;
