32b98a58d7cee2c7e58b3c5465a051b2ae0cecda
repotecc <repotecc@gmail.com>
2021-11-25 21:49:39 +0100

    fix bug
    Co-Authored-By: ayomide onibokun <35877863+ayo6706@users.noreply.github.com>
    Co-Authored-By: Adesanya Segun <65086989+raindas@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -14,9 +14,6 @@ function MyApp({ Component, pageProps }) {
   return(
 
         <>
-        <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,800;1,400;1,500;1,700&display=swap" rel="stylesheet" />
-        </Head>
         <Component {...pageProps} />
       </>
   );
