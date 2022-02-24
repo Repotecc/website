23f81e6fb1af7e0199790e2b2ebf60995501aebe
repotecc <35877863+ayo6706@users.noreply.github.com>
2022-02-25 00:08:54 +0100

    added             source: '/techfest-volunteers',

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   async redirects() {
     return [
         {
-            source: '/config',
-            destination: '/target',
+            source: '/techfest-volunteers',
+            destination: 'https://docs.google.com/forms/d/e/1FAIpQLSdqHwGY60GvX4Dy98Wp9uJD67YAQgnlm8G0u8B08j_gjXdi4g/viewform?usp=sf_link',
             permanent: false,
         }
     ]
