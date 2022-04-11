85d45849f9c2b68f7d5eb92164c0186d3506f816
repotecc <35877863+ayo6706@users.noreply.github.com>
2022-04-11 17:18:55 +0100

    added techfest link

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ module.exports = {
             source: '/techfest-volunteers',
             destination: 'https://docs.google.com/forms/d/e/1FAIpQLSdqHwGY60GvX4Dy98Wp9uJD67YAQgnlm8G0u8B08j_gjXdi4g/viewform?usp=sf_link',
             permanent: false,
+        },{
+            source: '/techfest22',
+            destination: 'https://docs.google.com/forms/d/e/1FAIpQLSePMMg8e8hpSCWd9zJYWBxQCo0Y5xE6ozR_FidUvWbN_jKtsw/viewform?usp=sf_link',
+            permanent: false,
         }
     ]
   }
--- pages/_middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-export async function middleware(req, ev) {
-    const { pathname } = req.nextUrl
-    if (pathname == '/techfest-volunteers') {
-        return NextResponse.redirect('https://docs.google.com/forms/d/e/1FAIpQLSdqHwGY60GvX4Dy98Wp9uJD67YAQgnlm8G0u8B08j_gjXdi4g/viewform?usp=sf_link')
-    }
-    return NextResponse.next()
-}
