fdc9b42010ab5e9967ca9259fee54703e019dc02
Ayomide Onibokun <ayo6706@gmail.com>
2024-03-30 23:14:49 +0100

    Add privacy and policy page

--- components/Home/Mainone.js
+++ components/Home/Mainone.js
@@ -6,27 +6,27 @@ import Link from 'next/link';
 
 function Mainone() {
   return (
-    <main  className="container">
+      <main  className="container">
 
-        <div  className="row nav-clear-margin section-1">
-            <div  className="col-md-4">
-                <h1><b>Find Your  <br /> Way In Tech</b></h1>
-                <p>
-                    We&apos;ll help you get started with <b>creative</b> and <b>innovative</b> skills. We&apos;ll help you get started in tech!
-                </p>
-                <button>
-                     <Link target="_blank" rel="noopener noreferrer"  href="https://forms.gle/XSA4hZBoVsoSToKt6"> 
-                        <a> GET STARTED</a>
-                      </Link>
-                </button>
-            </div>
-            <div  className="col-md-8">
-                <Image alt="vector 1" src={image1}  className="img-fluid" />
-            </div>
-        </div>
+          <div  className="row nav-clear-margin section-1">
+              <div  className="col-md-4">
+                  <h1><b>Find Your  <br /> Way In Tech</b></h1>
+                  <p>
+                      We&apos;ll help you get started with <b>creative</b> and <b>innovative</b> skills. We&apos;ll help you get started in tech!
+                  </p>
+                  <button>
+                       <Link target="_blank" rel="noopener noreferrer"  href="https://forms.gle/XSA4hZBoVsoSToKt6"> 
+                           GET STARTED
+                        </Link>
+                  </button>
+              </div>
+              <div  className="col-md-8">
+                  <Image alt="vector 1" src={image1}  className="img-fluid" />
+              </div>
+          </div>
 
 
-    </main>
+      </main>
   );
 }
 
