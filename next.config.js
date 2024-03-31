032a3630cf6bea52a8f419fc9671762722763f76
Ayomide Onibokun <ayo6706@gmail.com>
2024-03-31 12:25:35 +0100

    Comment target in next.config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-        {
-            source: '/techfest-volunteers',
-            destination: 'https://docs.google.com/forms/d/e/1FAIpQLSdqHwGY60GvX4Dy98Wp9uJD67YAQgnlm8G0u8B08j_gjXdi4g/viewform?usp=sf_link',
-            permanent: false,
-        },{
-            source: '/techfest22',
-            destination: 'https://docs.google.com/forms/d/e/1FAIpQLSePMMg8e8hpSCWd9zJYWBxQCo0Y5xE6ozR_FidUvWbN_jKtsw/viewform?usp=sf_link',
-            permanent: false,
-        }
-    ]
-  }
+  
 }
