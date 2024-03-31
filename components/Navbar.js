8be31c9a7f580d1e20ec692d379483825a10eb56
Ayomide Onibokun <ayo6706@gmail.com>
2024-03-31 11:18:34 +0100

    set node version

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,4 @@
 import logo from '../public/assets/img/repotecc_logo.png';
-
-
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -9,26 +7,17 @@ function Navbar() {
       <div className="Navbar">
           <nav className="navbar navbar-expand-lg fixed-top">
               <div className="container">
-                  
-                      <Link href="/" className="navbar-brand">
-
-                          <Image alt="repotecc logo" src={logo} className="loo" width={150} height={51} />
-
-                      </Link>
-                  
+                  <Link href="/" className="navbar-brand">
+                      <Image alt="repotecc logo" src={logo} className="loo" width={150} height={51} />
+                  </Link>
                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                      <span  className="bi-list"></span>
+                      <span className="bi-list"></span>
                   </button>
 
                   <div className="collapse navbar-collapse ml-auto ms-auto" id="navbarsExampleDefault">
                       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                          
                           <li className="nav-item active">
-                              <Link
-                                  className="nav-link"
-                                  // 
-                                  href="/#why_join_us"
-                                  aria-current="page"> Why join us?</Link>
+                              <Link href="/#why_join_us" className="nav-link" aria-current="page">Why join us?</Link>
                           </li>
                           <li className="nav-item">
                               <Link href="/#hire_us" className="nav-link">Hire us</Link>
@@ -39,11 +28,7 @@ function Navbar() {
                           <li className="nav-item">
                               <Link href="/#workshops" className="nav-link">Our workshops</Link>
                           </li>
-                          {/* <li className="nav-item">
-                              <Link   href="/#partners"><a className="nav-link">Our partners</a></Link>
-                          </li> */}
                           <li className="nav-item">
-          
                               <Link href="/team" className="nav-link">Our team</Link>
                           </li>
                           <li className="nav-item">
@@ -54,7 +39,6 @@ function Navbar() {
                           </li>
                       </ul>
                       <div className="d-flex">
-                      
                           <a target="_blank" rel="noopener noreferrer" className="btn btn-outline-success" href="https://paystack.com/pay/repotecc">
                               DONATE
                           </a>
@@ -62,7 +46,6 @@ function Navbar() {
                   </div>
               </div>
           </nav>
-                
       </div>
   );
 }
